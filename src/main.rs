d5d48402aead2b58093fdff4b876ff598d6bd355
kevin <kevin91cooland@gmail.com>
2023-07-13 17:20:52 +0800

    0.1

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
 fn main() {
-    let s = String::from("hello");
-    takes_ownership(s); //将值传递给函数，会发生移动或者复制，同let语句。
-    //s的值被移到了函数中，所有权转移给了some_string,所以到这里s已经失效。
-    //println!("在move进函数后继续使用s:{}",s);  报错
-    let x  = 5;
-    makes_copy(x); //将x传递给函数，x是i32类型的，发生复制(copy),所以不会失效。
-} //这里，x先移出作用域(栈：后进先出)，然后是s，因为s的值早转移了，所以不会有特殊操作。
+    let s1 = gives_ownership();         // gives_ownership 将返回值
+                                        // 移给 s1
 
-fn takes_ownership(some_string: String) { //some_string进入作用域。
-    println!("{}", some_string);
-} //这里, some_string移出作用域并调用"drop"方法,占用的内存被释放。
+    let s2 = String::from("hello");     // s2 进入作用域
 
-fn makes_copy(some_integer: i32) {
-    println!("{}",some_integer);
+    let s3 = takes_and_gives_back(s2);  // s2 被移动到
+                                        // takes_and_gives_back 中,
+                                        // 它也将返回值移给 s3
+} // 这里, s3 移出作用域并被丢弃。s2 也移出作用域，但已被移走，
+  // 所以什么也不会发生。s1 移出作用域并被丢弃
+
+fn gives_ownership() -> String {             // gives_ownership 将返回值移动给
+                                             // 调用它的函数
+
+    let some_string = String::from("hello"); // some_string 进入作用域.
+
+    some_string                              // 返回 some_string 并移出给调用的函数
+}
+
+// takes_and_gives_back 将传入字符串并返回该值
+fn takes_and_gives_back(a_string: String) -> String { // a_string 进入作用域
+
+    a_string  // 返回 a_string 并移出给调用的函数
 }
